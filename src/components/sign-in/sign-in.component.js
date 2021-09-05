ed548e68f5d6ed5511178559c70deda5a0701a6a
heatwaveo8 <217205+heatwaveo8@users.noreply.github.com>
2021-09-04 17:25:54 -0700

    setting google button to type equals button

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -49,7 +49,7 @@ class SignIn extends React.Component {
                     <FormInput handleChange={ this.handleChange } label='Password' name='password' type='password' value={ this.state.password } required />
                     <div className='buttons'>
                         <CustomButton type='submit'>Sign in</CustomButton>
-                        <CustomButton onClick={ signInWithGoogle } isGoogleSignIn>Sign in with Google</CustomButton>
+                        <CustomButton type='button' onClick={ signInWithGoogle } isGoogleSignIn>Sign in with Google</CustomButton>
                     </div>
                 </form>
             </div>
