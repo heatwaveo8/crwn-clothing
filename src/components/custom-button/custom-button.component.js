9e050ada00a6e4d2434e300674ed3728cd8e8e05
heatwaveo8 <217205+heatwaveo8@users.noreply.github.com>
2021-09-06 18:24:37 -0700

    updated shop data, and added cart util file to group items in cart reducer

--- src/components/custom-button/custom-button.component.js
+++ src/components/custom-button/custom-button.component.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
-    <button className={`custom-button ${isGoogleSignIn ? 'google-sign-in' : ''}`} { ...otherProps }>{ children }</button>
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
+    <button className={`custom-button ${inverted ? 'inverted' : 'google-sign-in'}`} { ...otherProps }>{ children }</button>
 );
 
 export default CustomButton;
