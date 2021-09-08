7a5912901618abff4c271d2aaf8b84c3c60ec3c0
heatwaveo8 <217205+heatwaveo8@users.noreply.github.com>
2021-09-07 17:52:11 -0700

    added addItem and removeItem functionality to checkout-item component

--- src/components/collection-item/collection-item.component.js
+++ src/components/collection-item/collection-item.component.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import CustomButton from './../custom-button/custom-button.component';
-import { addItemToCart } from './../../redux/cart/cart.actions'
+import { addItem } from './../../redux/cart/cart.actions'
 
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ item, addItemToCart }) => {
+const CollectionItem = ({ item, addItem }) => {
     const { name, imageUrl, price } = item;
 
     return (
@@ -18,13 +18,13 @@ const CollectionItem = ({ item, addItemToCart }) => {
                 <span className='name'>{ name }</span>
                 <span className='price'>{ price }</span>
             </div>
-            <CustomButton inverted onClick={ () => addItemToCart(item) }>Add to cart</CustomButton>
+            <CustomButton inverted onClick={ () => addItem(item) }>Add to cart</CustomButton>
         </div>
     );
 };
 
 const mapDispatchToProps = dispatch => ({
-    addItemToCart: item => dispatch(addItemToCart(item))
+    addItem: item => dispatch(addItem(item))
 });
 
 export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types';
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
 
 const INITIAL_STATE = {
     hidden: true,
@@ -13,10 +13,15 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                 ...state,
                 hidden: !state.hidden
             };
-        case CartActionTypes.ADD_ITEM_TO_CART:
+        case CartActionTypes.ADD_ITEM:
             return {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload)
+            };
+        case CartActionTypes.REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
             };
         case CartActionTypes.CLEAR_ITEM_FROM_CART:
             return {
