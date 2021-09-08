a8640b07602e1c702063871cf2cad54d3cfc7cdd
heatwaveo8 <217205+heatwaveo8@users.noreply.github.com>
2021-09-07 17:12:50 -0700

    removing items from cart

--- src/components/collection-item/collection-item.component.js
+++ src/components/collection-item/collection-item.component.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import CustomButton from './../custom-button/custom-button.component';
-import { addItem } from './../../redux/cart/cart.actions'
+import { addItemToCart } from './../../redux/cart/cart.actions'
 
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ item, addItem }) => {
+const CollectionItem = ({ item, addItemToCart }) => {
     const { name, imageUrl, price } = item;
 
     return (
@@ -18,13 +18,13 @@ const CollectionItem = ({ item, addItem }) => {
                 <span className='name'>{ name }</span>
                 <span className='price'>{ price }</span>
             </div>
-            <CustomButton inverted onClick={ () => addItem(item) }>Add to cart</CustomButton>
+            <CustomButton inverted onClick={ () => addItemToCart(item) }>Add to cart</CustomButton>
         </div>
     );
 };
 
 const mapDispatchToProps = dispatch => ({
-    addItem: item => dispatch(addItem(item))
+    addItemToCart: item => dispatch(addItemToCart(item))
 });
 
 export default connect(null, mapDispatchToProps)(CollectionItem);
